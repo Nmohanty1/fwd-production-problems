d1e37a4ab63aba638f4750a54e5622327425e101
Nikhil Mohanty <nmohanty1@hawk.iit.edu>
2018-11-12 01:13:10 -0600

    added javascript to make a button, can't make button work

--- pp-04/site.js
+++ pp-04/site.js
@@ -2,3 +2,6 @@ function ringDoorbell() {
   var doorbell = new Audio('media/doorbell.mp3');
   doorbell.play();
 }
+
+document.querySelector("#button").innerHTML= "<button id='play'>Play</button>"
+documennt.querySelector("#ring").addEventListener("click",ringDoorbell)
