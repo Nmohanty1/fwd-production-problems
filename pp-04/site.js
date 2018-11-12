b8d4b2ac34ef779a2bb86b6784a4a90fbe3b3112
Nikhil Mohanty <nmohanty1@hawk.iit.edu>
2018-11-12 01:20:15 -0600

    got button to link with sound and it plays

--- pp-04/site.js
+++ pp-04/site.js
@@ -1,7 +1,9 @@
+
 function ringDoorbell() {
   var doorbell = new Audio('media/doorbell.mp3');
   doorbell.play();
 }
 
+/*needed to create a button along with adding listner to play doorbell*/
 document.querySelector("#button").innerHTML= "<button id='play'>Play</button>"
-documennt.querySelector("#ring").addEventListener("click",ringDoorbell)
+document.querySelector("#play").addEventListener("click",ringDoorbell)
